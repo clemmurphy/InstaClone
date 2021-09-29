7a371d0311430d46d239e75f23d87c98a51ce474
bader <badermuteb2020@gmail.com>
2021-09-29 09:32:01 +0100

    updating the post controller and router with add, update and delete functionalties

--- server/config/router.js
+++ server/config/router.js
@@ -1,9 +1,14 @@
 import express from 'express'
-import { getAllPosts } from '../controllers/posts.js'
+import { getAllPosts, addPost, updatePost, getSinglePostById, deletePost } from '../controllers/posts.js'
 
 const router = express.Router()
 
 router.route('/posts')
   .get(getAllPosts)
+  .post(addPost)
+router.route('/posts/:id')
+  .get(getSinglePostById)
+  .put(updatePost)
+  .delete(deletePost)
 
 export default router
