744516aae820fa9ededd47620cbbe53e27baf883
Clement Murphy <clemmurphy@gmail.com>
2021-11-03 14:39:29 +0000

    Fixing port issues

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,5 +1,5 @@
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = function (app){
-  app.use(createProxyMiddleware('/api', { target: `http://localhost:${process.env.PORT || 4000}` }))
+  app.use(createProxyMiddleware('/api', { target: `${process.env.PORT}` || 'http://localhost:4000' }))
 }
