2316103c51315c236794363ff27113eb6083ae68
Clement Murphy <clemmurphy@gmail.com>
2021-11-03 14:22:44 +0000

    Setting up MongoDB Atlas

---
+++ data/insert-data.js
@@ -0,0 +1,42 @@
+import { MongoClient } from 'mongodb'
+
+// Replace the following with your Atlas connection string                                                                                                                                        
+const url = 'mongodb+srv://clemmurphy:...1derbaR@cluster0.kuawn.mongodb.net/Insta-Clone?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true'
+const client = new MongoClient(url)
+
+// The database to use
+const dbName = 'Insta-Clone'
+                      
+async function run() {
+  try {
+    await client.connect()
+    console.log('Connected correctly to server')
+    const db = client.db(dbName)
+
+    // Use the collection "people"
+    const col = db.collection('people')
+
+    // Construct a document                                                                                                                                                              
+    const personDocument = {
+      'name': { 'first': 'Alan', 'last': 'Turing' },
+      'birth': new Date(1912, 5, 23), // June 23, 1912                                                                                                                                 
+      'death': new Date(1954, 5, 7),  // June 7, 1954                                                                                                                                  
+      'contribs': [ 'Turing machine', 'Turing test', 'Turingery' ],
+      'views': 1250000
+    }
+
+    // Insert a single document, wait for promise so we can read it back
+    const p = await col.insertOne(personDocument)
+    // Find one document
+    const myDoc = await col.findOne()
+    // Print to the console
+    console.log(myDoc)
+
+  } catch (err) {
+    console.log(err.stack)
+  } finally {
+    await client.close()
+  }
+}
+
+run().catch(console.dir)
